c59db2e65680ff306572906ee9c1c2dad0c244d7
lrocoiers <romero.lucaas.lr@gmail.com>
2023-03-20 20:22:31 -0300

    5ta Modificacion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo :D");
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/api', (req, res) => {
+    console.log("pase por aca");
+    res.json({ mensaje : 'hola mundo'}).status(200)
+
+});
+
+app.listen(port, ()=>{console.log('Estoy escuchando en el puerto:'+ port)});
+
